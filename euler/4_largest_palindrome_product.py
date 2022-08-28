7348482c7df7de06a19757e8a5f33750787d6ee1
Lewis Chen <havkonstudio@gmail.com>
2022-08-28 01:44:14 -0800

    master

--- euler/4_largest_palindrome_product.py
+++ euler/4_largest_palindrome_product.py
@@ -7,40 +7,63 @@ LICENSE file in the root directory of this source tree.
 """
 import time
 
-def is_palindrome(number):
-    a = [x for x in str(number)]
-    b = int(len(a) / 2)
-    c = 0
-    for i in range(b):
-        if a[i] == a[-i-1]:
-            c += 1
-    if c == b:
-        return True
-    return False
-
-def largest_palindrome_product(number):
+def largest_palindrome_product_1(number):
     """
     A palindromic number reads the same both ways. The largest palindrome 
     made from the product of two 2-digit numbers is 9009 = 91 x 99.
     Find the largest palindrome made from the product of two 3-digit numbers.
+    This is a slightly slower version
     """
+    t = time.time()
     smallest = int(number * 0.1)
     largest = 0
     get_i = 0
     get_j = 0
     for i in range(number, smallest, -1):
+        for j in range(i, smallest, -1):
+            if str(i * j) == str(i * j)[::-1] and i * j >= largest:
+                largest = i * j
+                get_i = i
+                get_j = j
+                break
+    elapsed = time.time() - t
+    print('elapsed ', elapsed)
+    print(f'product is {get_i} x {get_j}')
+    print(f'palindrome is {largest}')
+
+
+def largest_palindrome_product_2(number):
+    """
+    A palindromic number reads the same both ways. The largest palindrome 
+    made from the product of two 2-digit numbers is 9009 = 91 x 99.
+    Find the largest palindrome made from the product of two 3-digit numbers.
+    This is a slightly faster version
+    """
+    t = time.time()
+    smallest = int(number * 0.1)
+    largest = 0
+    get_i = 0
+    get_j = 0
+    count = 0
+    for i in range(number, smallest, -1):
         found_palindrome = False
-        for j in range(i, 99, -1):
-            if is_palindrome(i * j):
+        for j in range(i, smallest, -1):
+            if str(i * j) == str(i * j)[::-1]:
                 found_palindrome = True
                 break
-        if found_palindrome and len(str(j)) == len(str(number)) and \
-            i * j >= largest:
+        if found_palindrome and i * j >= largest:
             largest = i * j
             get_i = i
             get_j = j
-    print(f'palindrome is {get_i} x {get_j}')
-    print(f'product is {largest}')
+            count += 1
+        # this assumes there is always a palindrome greater than this
+        if largest >= number**2 * 0.99:
+            break
+    elapsed = time.time() - t
+    print('elapsed ', elapsed)
+    print(f'product is {get_i} x {get_j}')
+    print(f'palindrome is {largest}')
+
 
 def quick_sort(arr):
 
@@ -69,5 +92,5 @@ def quick_sort(arr):
     arr = left + [arr[current_index]] + right #Merging everything together
     return arr
 
-largest_palindrome_product(999)
-# quick_sort([4,2,7,3])
+# largest_palindrome_product_1(9999)
+largest_palindrome_product_2(9999)
