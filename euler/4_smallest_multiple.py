54311908b36494220b0501d63480e5a36ccd0a0f
Lewis Chen <havkonstudio@gmail.com>
2022-08-27 23:24:42 -0800

    master

--- euler/4_smallest_multiple.py
+++ euler/4_smallest_multiple.py
@@ -13,13 +13,16 @@ def smallest_multiple_1(initial, start, end):
     from 1 to 10 without any remainder.
     What is the smallest positive number that is evenly divisible by all of the 
     numbers from 1 to 20?
+    This is algorithm is very optimized
     """
     a = initial
     t = time.time()
     primes = []
     not_primes = []
     common_factors = []
-    for i in range(start, end):
+
+    # Obtain the list of primes, non-primes and common factors
+    for i in range(start, end + 1):
         is_prime = True
         for j in range(2, i):
             if i % j == 0:
@@ -29,31 +32,31 @@ def smallest_multiple_1(initial, start, end):
             primes.append(i)
         else:
             not_primes.append(i)
-            common_factors = get_common_factors(common_factors, not_primes)
+            for i in not_primes:
+                for j in range(1, i+1):
+                    if i % j == 0 and j not in common_factors:
+                        common_factors.append(j)
     
+    # Multiply the number by the primes since they don't share common factors
     for i in primes:
         a = a * i
     
+    # If the value is evenly divisible then break the while loop
+    # Add the last prime to the initial value
     while a > 0:
         b = 0
-        for j in range(1, end):
+        for j in range(1, end + 1):
             if a % j == 0:
                 b += 1
-        if b == end - 1:
+        if b == end:
             break
+        # this step greatly shortens the time
         a += primes[-1]
     elapsed = time.time() - t
     print('elapsed ', elapsed)
-    print('result: ', a)
-    for i in range(1, end):
+    for i in range(1, end + 1):
         print(f'a / {i}: ', a / i)
-
-def get_common_factors(common_factors: list, not_primes: list) -> list:
-    for i in not_primes:
-        for j in range(1, i+1):
-            if i % j == 0 and j not in common_factors:
-                common_factors.append(j)
-    return common_factors
+    print('result: ', a)
 
 
 def smallest_multiple_2(initial, end):
@@ -69,10 +72,10 @@ def smallest_multiple_2(initial, end):
     t = time.time()
     while a > 0:
         b = 0
-        for i in range(1, end):
+        for i in range(1, end + 1):
             if a % i == 0:
                 b += 1
-        if b == end - 1:
+        if b == end:
             break
         a += 1
     elapsed = time.time() - t
@@ -81,6 +84,6 @@ def smallest_multiple_2(initial, end):
     print(f'a / {b}: ', a / b)
 
 a = 2520
-end = 21
-# smallest_multiple_1(a, 11, end)
-smallest_multiple_2(a, end)
+end = 22 #
+smallest_multiple_1(a, 11, end)
+# smallest_multiple_2(a, end)
